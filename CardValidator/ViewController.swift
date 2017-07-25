8ec7185f6f4beb66c659e4c46dea10d88527a593
Akhilesh Gandotra <akhilesh.gandotra@click-labs.com>
2017-07-25 13:03:17 +0530

    Initial Commit

---
+++ CardValidator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CardValidator
+//
+//  Created by soc-macmini-45 on 25/07/17.
+//  Copyright © 2017 soc-macmini-45. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
